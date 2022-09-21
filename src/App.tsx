0e2a2b9568b678190fe6da81633e6a1ef98f028b
Arthur Vinicius <thuur.vss@gmail.com>
2022-09-20 22:30:34 -0300

    wip:  antes do card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import bubaImg from "../src/assets/buba.png";
 import charmImg from "../src/assets/charm.png";
 import squirtleImg from "../src/assets/squirtle.png";
+import { Container } from "./assets/App";
 
 type FotoPokemon = {
   bubassauro: string;
@@ -17,16 +18,19 @@ function App() {
 
   const pokemons = [
     {
+      id: 1,
       nome: "bubassauro",
       forca: 100,
       tipo: "planta",
     },
     {
+      id: 2,
       nome: "squirtle",
       forca: 110,
       tipo: "agua",
     },
     {
+      id: 3,
       nome: "chamander",
       forca: 220,
       tipo: "fogo",
@@ -34,17 +38,19 @@ function App() {
   ];
 
   return (
-    <>
+    <ul>
       {pokemons.map((pokemon) => {
         return (
-          <div>
-            <h1>{pokemon.nome}</h1>
-            <span>Força: {pokemon.forca}</span>
-            <img src={fotoPokemon[pokemon.nome]} />
-          </div>
+          <li key={pokemon.id}>
+            <Container>
+              <h2>{pokemon.nome}</h2>
+              <span>Força: {pokemon.forca}</span>
+              <img src={fotoPokemon[pokemon.nome]} />
+            </Container>
+          </li>
         );
       })}
-    </>
+    </ul>
   );
 }
 
---
+++ src/assets/App.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: #ffffff;
+  padding: 10px;
+  height: 200px;
+  width: 250px;
+
+  border-radius: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.08);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
